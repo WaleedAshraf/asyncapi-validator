cd24a3878b05c3a8b64c126049b678244471e7d4
WaleedAshraf <waleedashraf@outlook.com>
2019-07-03 17:21:09 +0200

    improvemenst

--- README.md
+++ README.md
@@ -5,45 +5,23 @@ message validator through asyncapi
 
 
 ```javascript
-var Validator = require('asyncapi-validator')
-var v = new Validator('./api.yaml')
-v.validate('Key', {1:1})
-```
-
-Defination:
-```javascript
-/**
- * @param {String} path - local path or url of AsyncAPI schema (yaml or JSON)
- * @param {String} options - optional
- * @param {Object} callback - optional
- * @return {Validator} Validator instance
- */
-constructor Validator(<path>, <options>, <callback>)
+let Validator = require('asyncapi-validator')
+let validator = await Validator.loadFromFile('./api.yaml')
+validator.validate('Key', {1:1})
 ```
 
 Methods:
 ```javascript
-var v = new Validator('./api.yaml')
-
-v.path // ./api.yaml
-v.schema // JSON formated AsyncAPI schema
-v.asyncapi // AsyncApi schema defination
-v.validator // ojbect with all validation resolved
-
+loadFromFile('./api.yaml')
+loadFromUrl('https://example.com/api.yml')
 ```
 
 Example:
 ```javascript
-/**
- * @param {String} path - local path or url of AsyncAPI schema (yaml or JSON)
- * @param {String} key - key to validate payload against
- * @param {Object} payload - payload of message
- */
 v.validate('UserCreated',
   {
     userId: 'bd58d14f-fd3e-449c-b60c-a56548190d68',
     createdAt: 'bd58d14f-fd3e-449c-b60c-a56548190d69',
   }
 )
-
 ```
--- index.js
+++ index.js
@@ -5,70 +5,66 @@ const yaml = require('yamljs')
 const axios = require('axios')
 const fs = require('fs')
 const asyncapi = require('asyncapi')
+const util = require('util')
+const {extname} = require('path')
 
-const extRegex = '\.[^.\\/:*?"<>|\r\n]+$'
-const httpRegex = '^(http|https):\/\/'
+// Convert fs.readFile into Promise version
+const readFile = util.promisify(fs.readFile)
+
+let v = null
 
 class Validate {
-  constructor(path, options, callback) {
 
-    if (!path)
-      throw new Error('path not defined')
-    this.path = path
-    this.options = options
-    this.callback = callback
+  constructor() {
+    this.path = null
     this.schema = null
     this.asyncapi = null
     this.validator = {}
-    this.Ajv = Ajv
-    this.getSchema()
-    this.loadAsyncApi()
-    this.validateSchema()
-    this.creatValidator()
+    this.schemaAjv = null
+    this.validateAjv = null
   }
 
   loadAsyncApi() {
     this.asyncapi = asyncapi[this.schema.asyncapi]
   }
 
+  // validate user defined AsyncApi schema against AsyncApi schema defination
   validateSchema() {
-    const ajv = new this.Ajv({schemaId: 'auto'})
-    const valid = ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'))
-    if (!valid) console.log(ajv.errors);
-  }
-
-  validate(key, payload) {
-    const ajv = new this.Ajv({allErrors: true})
-    const validate = ajv.compile(this.validator[key])
-    const valid = validate(payload)
-    if (valid) console.log('Valid!')
-    else console.log('Invalid: ' + ajv.errorsText(validate.errors))
-  }
-
-  getSchema() {
-    const [ext] = this.path.match(extRegex)
-    let schema = null
-    if (this.path.match(httpRegex)) {
-      this.getFile(this.path).then((res) => {
-        if (ext === '.json') {
-          schema = res
-        } else if (ext === '.yaml' || ext === '.yml') {
-          schema = yaml.parse(res)
-        }
-        this.schema = schema
-      })
-    } else {
-      if (ext === '.json') {
-        fs.readFile(this.path, 'utf8', (err, data) => {
-          if (err) throw err
-          this.schema = JSON.parse(data)
-        })
-      } else if (ext === '.yaml' || ext === '.yml') {
-        this.schema = yaml.load(this.path)
-      }
+    if (!this.schemaAjv) {
+      this.schemaAjv = new Ajv({schemaId: 'auto', allErrors: true})
+      this.schemaAjv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'))
+    }
+    const validate = this.schemaAjv.compile(this.asyncapi)
+    const valid = validate(this.schema)
+    if (!valid) {
+      console.log('Invalid: ' + this.schemaAjv.errorsText(validate.errors))
+      throw new Error(this.schemaAjv.errorsText(validate.errors))
     }
   }
 
+
+  /**
+   * @param {string} key
+   * @param {Object} payload
+   * validate "Payload" using the provided "Key" against generated schema
+   */
+  validate(key, payload) {
+    if (!this.validateAjv) {
+      this.validateAjv = new Ajv({allErrors: true})
+    }
+    const validate = this.validateAjv.compile(this.validator[key])
+    const valid = validate(payload)
+    if (valid) {
+      console.log('Valid!')
+    } else {
+      console.log('Invalid: ' + this.validateAjv.errorsText(validate.errors))
+      throw new Error(this.validateAjv.errorsText(validate.errors))
+    }
+  }
+
+  /**
+   * @param {any} url
+   */
   async getFile(url) {
     try {
       const {data} = await axios.get(url)
@@ -79,13 +75,17 @@ class Validate {
     }
   }
 
-
-  creatValidator() {
+  createValidator() {
     for (let key in this.schema.components.messages) {
       this.validator[key] = this.resolveRef(this.schema.components.messages[key].payload, this.schema)
     }
   }
 
+  /**
+   * @param {Object} data - data
+   * @param {Object} schema - compiled schema
+   * resolve all ref recursively
+   */
   resolveRef(data, schema) {
     for (let key in data) {
       if (key === '$ref') {
@@ -105,6 +105,60 @@ class Validate {
     }
     return data
   }
+
+  init() {
+    this.loadAsyncApi()
+    this.validateSchema()
+    this.createValidator()
+    return
+  }
 }
 
-module.exports = Validate
+/**
+ * @param {string} path
+ */
+const getInstance = (path) => {
+  if (!v || path !== v.path)
+    v = new Validate()
+  return v
+}
+
+/**
+ * @param {string} path
+ * load schema from file and parse to JSON
+ */
+const loadFromFile = async (path) => {
+  const apv = getInstance(path)
+
+  apv.path = path
+  const ext = extname(path)
+  let data = await readFile(path, 'utf8')
+  if (ext === '.json') {
+    apv.schema = JSON.parse(data)
+  } else if (ext === '.yaml' || ext === '.yml') {
+    apv.schema = yaml.load(apv.path)
+  }
+  apv.init()
+  return apv
+}
+
+/**
+ * @param {string} path - path of asyncapi schema
+ * load schema from url and parse to JSON
+ */
+const loadFromUrl = async (path) => {
+  const apv = getInstance(path)
+
+  apv.path = path
+  const ext = extname(path)
+  let data = await apv.getFile(apv.path)
+  if (ext === '.json') {
+    apv.schema = data
+  } else if (ext === '.yaml' || ext === '.yml') {
+    apv.schema = yaml.parse(data)
+  }
+  apv.init()
+  return apv
+}
+
+module.exports = {loadFromFile, loadFromUrl}
