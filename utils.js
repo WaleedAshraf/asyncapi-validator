3ed368bed8fdd683550b672ba2773e839ab978be
WaleedAshraf <waleedashraf@outlook.com>
2019-07-06 21:54:33 +0200

    added JSDocs

--- utils.js
+++ utils.js
@@ -1,10 +1,18 @@
 const YAML_EXTENSIONS = ['.yml', '.yaml']
 
 module.exports = {
+  /**
+   * @param {string} extension
+   * @returns {boolean}
+   */
   isJson(extension) {
     return extension === '.json'
   },
 
+  /**
+   * @param {string} extension
+   * @returns {boolean}
+   */
   isYaml(extension) {
     return YAML_EXTENSIONS.includes(extension)
   }
