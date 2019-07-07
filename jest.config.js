4af2f2ae914fd452823b9b9fb3e9c1a504eb4f1d
WaleedAshraf <waleedashraf@outlook.com>
2019-07-07 22:51:01 +0200

    added all paths

--- jest.config.js
+++ jest.config.js
@@ -113,8 +113,11 @@ module.exports = {
 
   // A list of paths to directories that Jest should use to search for files in
   roots: [
-    '<rootDir>',
-    '<rootDir>/src/'
+    '<rootDir>/',
+    '<rootDir>/src',
+    '<rootDir>/parsers',
+    '<rootDir>/loaders',
+    '<rootDir>/utils'
   ],
 
   modulePaths: [
