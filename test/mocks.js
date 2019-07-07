c69b11096f681487ed5ca71c3ef90a969c48757f
WaleedAshraf <waleedashraf@outlook.com>
2019-07-07 22:25:16 +0200

    added mocks

---
+++ test/mocks.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const mocks = {
+  slack: 'https://raw.githubusercontent.com/asyncapi/asyncapi/master/examples/1.2.0/slack-rtm.yml',
+  streetlights: 'https://raw.githubusercontent.com/asyncapi/asyncapi/master/examples/1.2.0/streetlights.yml',
+  htmlFile: 'https://github.com/asyncapi/asyncapi/blob/master/examples/1.2.0/slack-rtm.yml'
+}
+
+module.exports = mocks
