760707d062d17ebc4bb4fc207e70afe066027ff5
WaleedAshraf <waleedashraf@outlook.com>
2019-07-07 22:35:46 +0200

    fixed module path for jest

--- jest.config.js
+++ jest.config.js
@@ -113,8 +113,12 @@ module.exports = {
 
   // A list of paths to directories that Jest should use to search for files in
   // roots: [
-  //   "<rootDir>"
+  //   '<rootDir>'
   // ],
+
+  modulePaths: [
+    '<rootDir>'
+  ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
