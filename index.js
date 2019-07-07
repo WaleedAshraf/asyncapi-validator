23888615d3242547c5385428483c0cfa76c91a45
WaleedAshraf <waleedashraf@outlook.com>
2019-07-08 00:43:42 +0200

    buid dir

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 'use strict'
-
+console.log('process.env.TRAVIS_BUILD_DIR', process.env.TRAVIS_BUILD_DIR)
 const AsyncApiValidatorFactory = require('./src/AsyncApiValidatorFactory')
 module.exports = new AsyncApiValidatorFactory()
