e4886e7cc44f6c8ef38ef88e71598df3c220204b
WaleedAshraf <waleedashraf@outlook.com>
2019-07-08 15:01:21 +0200

    moved resolver to validator

--- src/AsyncApiValidator.js
+++ src/AsyncApiValidator.js
@@ -1,17 +1,19 @@
 const {fail} = require('assert')
 const Ajv = require('ajv')
+const deref = require('json-schema-deref-sync')
 
 class AsyncApiValidator {
   /**
    * @param {any} schema
-   * @param {any} asyncapi
-   * @param {Object} resolvedSchema
+   * @param {any} asyncapiSchema
    */
-  constructor(schema, asyncapi, resolvedSchema) {
+  constructor(schema, asyncapiSchema) {
     this._schema = schema || fail('schema is mandatory')
-    this._asyncapi = asyncapi || fail('asyncapi is mandatory')
-    this.resolvedSchema = resolvedSchema || fail('asyncapi is mandatory')
-    this.ajv = null
+    this._asyncapiSchema = asyncapiSchema || fail('asyncapi is mandatory')
+    this._ajv = new Ajv({allErrors: true})
+    validateSchema(schema, asyncapiSchema)
+    this._resolvedSchema = deref(schema, null)
+    this._messages = this._resolvedSchema.components.messages
   }
 
   /**
@@ -20,32 +22,34 @@ class AsyncApiValidator {
    * @returns {boolean}
    */
   validate(key, payload) {
-    if (!this.ajv)
-      this.ajv = new Ajv({allErrors: true})
-    if (!this.resolvedSchema.components.messages[key]) {
+    if (!this._messages[key]) {
       console.log(`key ${key} not found`)
       throw new Error(`key ${key} not found`)
     }
-    const validator = this.ajv.compile(this.resolvedSchema.components.messages[key].payload)
+
+    const validator = this._ajv.compile(this._messages[key].payload)
     const result = validator(payload)
+
     if (!result) {
-      console.log('Invalid: ' + this.ajv.errorsText(validator.errors))
-      throw new Error(this.ajv.errorsText(validator.errors))
+      console.log('Invalid: ' + this._ajv.errorsText(validator.errors))
+      throw new Error(this._ajv.errorsText(validator.errors))
     }
+
     return true
   }
+}
 
-  validateSchema() {
-    // validate user defined AsyncApi schema against AsyncApi schema defination
-    const ajv = new Ajv({schemaId: 'auto', allErrors: true})
-    // @ts-ignore
-    ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'))
-    const validate = ajv.compile(this._asyncapi)
-    const valid = validate(this._schema)
-    if (!valid) {
-      console.log('Invalid: ' + ajv.errorsText(validate.errors))
-      throw new Error(ajv.errorsText(validate.errors))
-    }
+const validateSchema = (schema, asyncapiSchema) => {
+  // validate user defined AsyncApi schema against AsyncApi schema defination
+  const ajv = new Ajv({schemaId: 'auto', allErrors: true})
+  ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'))
+
+  const validate = ajv.compile(asyncapiSchema)
+  const valid = validate(schema)
+
+  if (!valid) {
+    console.log('Invalid: ' + ajv.errorsText(validate.errors))
+    throw new Error(ajv.errorsText(validate.errors))
   }
 }
 
