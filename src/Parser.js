fb4c75dae0d807d500ec65839553198395a8d9df
WaleedAshraf <waleedashraf@outlook.com>
2019-07-14 01:05:01 +0200

    added jsrp parser

---
+++ src/Parser.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const jsrp = require('json-schema-ref-parser')
+
+class Parser {
+  /**
+   * @param {string} source
+   * @returns {JSON} ref resolved schema object
+   */
+  async parse(source) {
+    try {
+      const options = {
+        parse: {
+          yaml: {
+            allowEmpty: false
+          },
+          text: false,
+          binary: false
+        },
+        dereference: {
+          circular: 'ignore'
+        }
+      }
+      const schema = await jsrp.dereference(source, options)
+      return schema
+    } catch (err) {
+      console.error(`error while parsing schema ${err}, ${err.stack}`)
+      throw new Error(err)
+    }
+  }
+}
+
+module.exports = new Parser()
