5b985b580f07846495df75edf2364fd5c8e78b34
Waleed ASHRAF <waleedashraf@outlook.com>
2020-11-15 04:49:34 +0100

    add support for openapi formats
    break code in modules: _shouldHandleArray, _validateArgs

--- src/MessageValidator.js
+++ src/MessageValidator.js
@@ -1,4 +1,3 @@
-const {fail} = require('assert')
 const Ajv = require('ajv')
 const ValidationError = require('./ValidationError')
 
@@ -9,13 +8,11 @@ class MessageValidator {
    * @param {object} channels - simplified channels object from schema
    */
   constructor(schema, options, channels) {
-    this._schema = schema || fail('schema is mandatory')
-    this._messages = this._schema.components ? this._schema.components.messages : []
-    this._channels = channels
-    this._ajv = new Ajv({allErrors: true})
-    this._options = options
-
     this.schema = schema
+    this._messages = this.schema.components ? this.schema.components.messages : []
+    this._channels = channels
+    this._ajv = new Ajv({allErrors: true, unknownFormats: ['int32', 'int64', 'float', 'double', 'byte']})
+    this._options = options
   }
 
   /**
@@ -25,51 +22,40 @@ class MessageValidator {
    * @param {Object} operation
    * @returns {boolean}
    */
-  validate(key, payload, channel = null, operation = null) {
-    const shouldIgnoreArray = this._options.ignoreArray === true
-    let payloadSchema = null
-
-    if (channel) {
-      if (!operation || !(operation === 'publish' || operation === 'subscribe')) {
-        throw new ValidationError(`operation "${operation}" is not valid`)
-      }
-
-      if (!this._options.msgIdentifier) {
-        throw new ValidationError('"msgIdentifier" is required with channel validation')
-      }
-
-      if (!this._channels[channel]) {
-        throw new ValidationError(`channel "${channel}" not found`)
-      }
-
-      if (!this._channels[channel][operation][key]) {
-        throw new ValidationError(`message with key "${key}" on channel "${channel}" and operation "${operation}" not found`)
-      }
-
-      payloadSchema = this._channels[channel][operation][key].payload
-    } else {
-      if (!this._messages[key]) {
-        throw new Error(`key ${key} not found`)
-      }
-
-      payloadSchema = this._messages[key].payload
-    }
-
+  validate(key, payload, channel, operation) {
+    this._validateArgs(key, channel, operation)
+    const payloadSchema = this._channels[channel][operation][key].payload
+    payload = this._shouldHandleArray(payloadSchema, payload) ? [payload] : payload
     const validator = this._ajv.compile(payloadSchema)
-    const schemaIsArray = payloadSchema.type === 'array'
-    const payloadIsNotArray = !Array.isArray(payload)
-
-    if (shouldIgnoreArray && schemaIsArray && payloadIsNotArray) {
-      payload = [payload]
-    }
 
     const result = validator(payload)
 
-    if (!result) {
-      throw new ValidationError(this._ajv.errorsText(validator.errors), key, validator.errors)
-    }
+    if (!result) throw new ValidationError(this._ajv.errorsText(validator.errors), key, validator.errors)
 
     return true
+  }
+
+  _validateArgs(key, channel, operation) {
+    if (!operation || !(operation === 'publish' || operation === 'subscribe')) {
+      throw new ValidationError(`operation "${operation}" is not valid`)
+    }
+
+    if (!this._channels[channel]) {
+      throw new ValidationError(`channel "${channel}" not found`)
+    }
+
+    if (!this._channels[channel][operation][key]) {
+      throw new ValidationError(`message with key "${key}" on channel "${channel}" and operation "${operation}" not found`)
+    }
+  }
+
+  _shouldHandleArray(payloadSchema, payload) {
+    const shouldIgnoreArray = this._options.ignoreArray === true
+    const schemaIsArray = payloadSchema.type === 'array'
+    const payloadIsNotArray = !Array.isArray(payload)
+
+    if (shouldIgnoreArray && schemaIsArray && payloadIsNotArray) return true
+    return false
   }
 }
 
