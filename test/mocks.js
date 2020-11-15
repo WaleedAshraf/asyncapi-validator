bca495b684b90502db979905a837d6a3484bc852
Waleed ASHRAF <waleedashraf@outlook.com>
2020-11-15 04:55:57 +0100

    update test schemas to v2.0.0

--- test/mocks.js
+++ test/mocks.js
@@ -1,8 +1,8 @@
 
 const mocks = {
-  slack: 'https://raw.githubusercontent.com/asyncapi/asyncapi/master/examples/1.2.0/slack-rtm.yml',
-  streetlights: 'https://raw.githubusercontent.com/asyncapi/asyncapi/master/examples/1.2.0/streetlights.yml',
-  htmlFile: 'https://github.com/asyncapi/asyncapi/blob/master/examples/1.2.0/slack-rtm.yml'
+  slack: 'https://raw.githubusercontent.com/asyncapi/asyncapi/master/examples/2.0.0/slack-rtm.yml',
+  streetlights: 'https://raw.githubusercontent.com/asyncapi/asyncapi/master/examples/2.0.0/streetlights.yml',
+  htmlFile: 'https://github.com/asyncapi/asyncapi/blob/master/examples/2.0.0/slack-rtm.yml'
 }
 
 module.exports = mocks
